3f85ed928cdbb56f8ffc9ee63b0ae9c45938d8b4
wovlf02 <onlygithub02@gmail.com>
2025-03-13 14:11:29 +0900

    5장 진행 중

---
+++ BaekJoon/5/11720.py
@@ -0,0 +1,3 @@
+a = int(input())
+nums = list(map(int, input()))
+print(sum(nums))
---
+++ BaekJoon/5/9086.py
@@ -0,0 +1,5 @@
+a = int(input())
+for i in range(a):
+    str = input()
+    print(str[0], end="")
+    print(str[-1])
